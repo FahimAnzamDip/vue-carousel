b6f2e61e6a587a8063c322c1717447c6eaf95f73
Fahim Anzam Dip <dfahimanzam@gmail.com>
2023-07-23 18:53:59 +0600

    Bug fixed

--- src/data/images.js
+++ src/data/images.js
@@ -1,27 +1,27 @@
 const images = [
     {
         id: 1,
-        url: "/images/image-1.jpg",
+        url: "images/image-1.jpg",
         caption: "A Beautiful Scenery",
     },
     {
         id: 2,
-        url: "/images/image-2.jpg",
+        url: "images/image-2.jpg",
         caption: "A Beautiful Scenery",
     },
     {
         id: 3,
-        url: "/images/image-3.jpg",
+        url: "images/image-3.jpg",
         caption: "A Beautiful Scenery",
     },
     {
         id: 4,
-        url: "/images/image-4.jpg",
+        url: "images/image-4.jpg",
         caption: "A Beautiful Scenery",
     },
     {
         id: 5,
-        url: "/images/image-5.jpg",
+        url: "images/image-5.jpg",
         caption: "A Beautiful Scenery",
     },
 ];
